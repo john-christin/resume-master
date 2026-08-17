fe5a40cd9c20761998dde0e997c8f4a3480c41cf
john-christin <john.dev.137940@gmail.com>
2026-08-17 03:50:40 -0500

    feat: dockerize with postgres service and fix networking on macOS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,8 +18,8 @@ export default defineConfig({
       host: "localhost",
     },
     proxy: {
-      "/api": "http://localhost:8000",
-      "/uploads": "http://localhost:8000",
+      "/api": process.env.VITE_API_HOST || "http://localhost:8000",
+      "/uploads": process.env.VITE_API_HOST || "http://localhost:8000",
     },
   },
 });
